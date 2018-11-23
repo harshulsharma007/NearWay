c8fcf8406a5af1bccd44f0b3a8f95556b5decfcd
harshulsharma007 <sharma.14@iitj.ac.in>
2018-11-23 21:02:38 +0530

    chat UI update

--- lib/people/ChatDetail.dart
+++ lib/people/ChatDetail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:near_way/people/ChatMessage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class chatDetail extends StatefulWidget {
   @override
@@ -7,6 +9,10 @@ class chatDetail extends StatefulWidget {
 }
 
 class chatDetailState extends State<chatDetail> {
+
+  String userid;
+  var listmessage;
+
   final TextEditingController _textEditingController =
       new TextEditingController();
   final List<ChatMessage> _messages = <ChatMessage>[];
@@ -55,6 +61,98 @@ class chatDetailState extends State<chatDetail> {
             ],
           ),
         ));
+  }
+
+  bool isLastMessageRight(int index){
+    if((index > 0 && listmessage!=null && listmessage[index-1]['id'] != userid) || index == 0){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  bool isLastMessageLeft(int index){
+    if((index > 0 && listmessage!=null && listmessage[index-1]['id'] == userid) || index == 0){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  Widget buildItem(int index, DocumentSnapshot document){
+    if(document['id']==userid){
+      return Row(
+        children: <Widget>[
+          Container(
+            child: Text(
+              document['content'],
+              style: TextStyle(color: Colors.black),
+            ),
+            padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+            width: 200.0,
+            decoration: BoxDecoration(
+              color: Color(0xffE8E8E8),
+              borderRadius: BorderRadius.circular(8.0)
+            ),
+            margin: EdgeInsets.only(
+              bottom: isLastMessageRight(index) ? 20.0 : 10.0,
+              right: 10.0
+            ),
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.end,
+      );
+    }else {
+      return Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                isLastMessageLeft(index)
+                    ? Material(
+                  child: CachedNetworkImage(
+                    placeholder: Container(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 1.0,
+                        valueColor: AlwaysStoppedAnimation<Color>(Color(0xfff5a623)),
+                      ),
+                      width: 35.0,
+                      height: 35.0,
+                      padding: EdgeInsets.all(10.0),
+                    ),
+                    imageUrl: imageurl,
+                    width: 35.0,
+                    height: 35.0,
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(18.0)
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                )
+                    :
+                    Container(width: 35.0),
+
+                Container(
+                  child: Text(
+                    document['content'],
+                    style: TextStyle(color:Colors.white),
+                  ),
+                  padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                  width: 200.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xff203152),
+                    borderRadius: BorderRadius.circular(8.0)
+                  ),
+                  margin: EdgeInsets.only(left: 10.0),
+                )
+
+              ],
+            )
+          ],
+        ),
+      )
+    }
   }
 
   @override
